1bcf8854a2ee6bd472951874510d4992c52ae353
OmarKhaled2020-web <omarabdelaal2013@gmail.com>
2021-05-31 12:02:41 +0200

    Hello Omar

--- app/src/main/java/com/omar/arabtv/MainActivity.java
+++ app/src/main/java/com/omar/arabtv/MainActivity.java
@@ -1,11 +1,8 @@
 package com.omar.arabtv;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -21,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    //private Navigation navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        /*binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
@@ -48,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
     }
 
     @Override
--- app/src/main/java/com/omar/arabtv/Show_content.java
+++ app/src/main/java/com/omar/arabtv/Show_content.java
@@ -1,24 +1,30 @@
 package com.omar.arabtv;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.MediaController;
 
 import com.google.android.material.navigation.NavigationView;
-import com.omar.arabtv.databinding.ActivityMainBinding;
+
+import org.jetbrains.annotations.NotNull;
 
 
 public class Show_content extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle toggle;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +32,34 @@ public class Show_content extends AppCompatActivity {
         setContentView(R.layout.show_content);
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar_show_content);
+        toolbar.setTitle("القاهرة كابول");
         setSupportActionBar(toolbar);
 
+        navigationView = (NavigationView)findViewById(R.id.nav_view_content);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_content);
 
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        /*navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        startActivity(new Intent(Show_content.this,MediaPlayer.class));
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_gallery:
+                        startActivity(new Intent(Show_content.this,MainActivity.class));
+                        break;
+
+                }
+                return true;
+            }
+        });*/
 
     }
 
